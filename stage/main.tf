98b86e19dc758ed4a1ae14c81486368908de2e4c
Deejay Geroso <deejaygeroso@gmail.com>
2020-07-31 21:14:28 +0800

    Updated main.tf on stage directory

--- stage/main.tf
+++ stage/main.tf
@@ -6,7 +6,6 @@ module "main-vpc" {
 }
 
 module "instances" {
-  # AWS_ACCOUNT    = var.AWS_ACCOUNT
   ENV            = "dev"
   PUBLIC_SUBNETS = module.main-vpc.public_subnets
   VPC_ID         = module.main-vpc.vpc_id
